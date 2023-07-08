919d3e771246feffa416dac82982ca381f7606b8
shivam kumar <20051252@kiit.ac.in>
2023-07-08 19:10:19 +0530

    staic preview image added

--- src/components/RandomImage.js
+++ src/components/RandomImage.js
@@ -1,55 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-// import {FacebookShareButton, FacebookIcon, WhatsappShareButton, WhatsappIcon, TwitterShareButton, TwitterIcon} from 'react-share'
-
-// import './RandomImage.css'; // Update the file path to match your CSS file location
-
-// const RandomImage = () => {
-//   const [imageUrl, setImageUrl] = useState('');
-//   const [shareUrl, setShareUrl] = useState('');
-
-//   const fetchRandomImage = async () => {
-//     try {
-//       const response = await axios.get('https://picsum.photos/400');
-//       setImageUrl(response.request.responseURL);
-//     } catch (error) {
-//       console.error('Error fetching random image:', error);
-//     }
-//   };
-
-//   useEffect(() => {
-//     fetchRandomImage();
-//     setShareUrl(window.location.href);
-//   }, []);
-
-//   return (
-//         <div className='random-image-container'>
-//             {imageUrl !== '' ? <img src={imageUrl} alt="Random" /> : <p>Loading image...</p>}
-//             {console.log(shareUrl)}
-//             <FacebookShareButton 
-//                 url={shareUrl}
-//                 quote={'title goes here'}
-//             >
-//                 <FacebookIcon size={40} round={true}/>
-//             </FacebookShareButton>
-//             <WhatsappShareButton 
-//                 url={shareUrl}
-//                 quote={'title goes here'}
-//                 >
-//                 <WhatsappIcon size={40} round={true}/>
-//             </WhatsappShareButton>
-//             <TwitterShareButton 
-//                 url={shareUrl}
-//                 quote={'title goes here'}
-//                 >
-//                 <TwitterIcon size={40} round={true}/>
-//             </TwitterShareButton>
-//         </div>
-//   );
-// }
-
-// export default RandomImage;
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { FacebookShareButton, FacebookIcon, WhatsappShareButton, WhatsappIcon, TwitterShareButton, TwitterIcon } from 'react-share';
@@ -69,15 +17,26 @@ const RandomImage = () => {
     }
   };
 
+//   const updateMetaTag = (imageUrl) => {
+//     const metaTag = document.querySelector('meta[property="og:image"]');
+//     if (metaTag) {
+//       metaTag.content = imageUrl;
+//     }
+//   };
+  
+
   useEffect(() => {
     fetchRandomImage();
     setShareUrl(window.location.href);
   }, []);
 
+//   useEffect(() => {
+//     updateMetaTag(imageUrl);
+//   }, [imageUrl])
+
   return (
     <div className='random-image-container'>
       {imageUrl !== '' ? <img src={imageUrl} alt="Random" /> : <p>Loading image...</p>}
-      {console.log(shareUrl)}
       <div className='icon-container'>
         <FacebookShareButton url={shareUrl} quote={'title goes here'}>
           <FacebookIcon size={40} round={true} />
